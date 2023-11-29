43a4023c6c3aac9d03f8659f16565144f5b86e2a
VladimirSerkutyev <serkutsyevv@gmail.com>
2023-11-29 14:39:46 +0300

    second task

--- app/src/main/java/com/example/andersenhw2/MainActivity.kt
+++ app/src/main/java/com/example/andersenhw2/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.andersenhw2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
-import com.example.andersenhw2.databinding.ActivityMainBinding
 import com.example.andersenhw2.fragments.ImageScreenFragment
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
--- app/src/main/java/com/example/andersenhw2/fragments/ImageScreenFragment.kt
+++ app/src/main/java/com/example/andersenhw2/fragments/ImageScreenFragment.kt
@@ -1,12 +1,20 @@
 package com.example.andersenhw2.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.example.andersenhw2.R
 import com.example.andersenhw2.databinding.FragmentImageScreenBinding
 
 class ImageScreenFragment : Fragment() {
@@ -28,18 +36,65 @@ class ImageScreenFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         with(binding) {
-            uploadButton.setOnClickListener {
-                Glide.with(requireActivity())
-                    .load(editText.text.toString())
-                    .into(imageView)
-            }
+            editText.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        val url = s.toString()
+                        loadImage(url)
+                    }, 1000)
+                }
+
+                override fun afterTextChanged(s: Editable?) {
+                }
+            })
         }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun loadImage(url: String) {
+        Glide.with(this)
+            .load(url)
+            .placeholder(R.drawable.ic_launcher_foreground)
+            .error(R.drawable.ic_launcher_foreground)
+            .listener(object : RequestListener<Drawable> {
+
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: com.bumptech.glide.request.target.Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    Toast.makeText(
+                        requireContext(),
+                        "Oops... Something went wrong",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: com.bumptech.glide.request.target.Target<Drawable>?,
+                    dataSource: com.bumptech.glide.load.DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+            })
+            .into(binding.imageView)
     }
 }
